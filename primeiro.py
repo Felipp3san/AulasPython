bbec5caa329340b17ebba9234dd91e4cf44013a9
Felippe Santana <f-elipp-3@hotmail.com>
2023-12-05 15:16:43 +0000

    Aula 2 de Python

---
+++ primeiro.py
@@ -0,0 +1,38 @@
+# Escrever no console
+
+print("Ola Mundo")
+
+# Tipos primitivos Python
+idade = 10
+altura = 2.599
+nome = "Felippe"
+estudante = True
+
+print(type(altura))
+print(type(idade))
+print(type(nome))
+print(type(estudante))
+
+# Comentário simples
+
+'''
+Comentário em bloco
+Teste
+Um
+Dois
+Três
+'''
+
+# Comentários em variáveis
+
+email = '''
+Olá,
+Esse é um teste 
+de comentários
+em variáveis
+'''
+
+print(email)
+print(type(email))
+
+
